a090db02d0d9833ec14c47ab90cc6d370de40d21
Luan Silveira <luan.w.silveira@gmail.com>
2019-04-13 15:41:14 -0300

    Update main.js

--- bible-projector/main.js
+++ bible-projector/main.js
@@ -1,8 +1,19 @@
-const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
 
 function createWindow() {
     // Create the browser window.
-    let win = new BrowserWindow({ title: 'Bíblia projector', width: 800, height: 600, autoHideMenuBar: true, icon: './data/icon.png' });
+    const { width, height } = electron.screen.getPrimaryDisplay().size;
+
+    let win = new BrowserWindow({
+        title: 'Bíblia projector',
+        width: 800, height: 600,
+        autoHideMenuBar: true,
+        icon: './data/icon.png',
+        minWidth: 600, minHeight: 500
+    });
+
+    win.maximize();
 
     // and load the index.html of the app.
     win.loadFile('index.html');
