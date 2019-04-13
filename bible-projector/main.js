327837312fc7293db196b1f9eae496a917999cc7
Luan Silveira <luan.w.silveira@gmail.com>
2019-04-13 12:14:57 -0300

    erro de escrita ajuda consertado

--- bible-projector/main.js
+++ bible-projector/main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
     // Create the browser window.
-    let win = new BrowserWindow({ width: 800, height: 600, autoHideMenuBar: true, icon: 'data/icon.png' });
+    let win = new BrowserWindow({ width: 800, height: 600, autoHideMenuBar: true, icon: './data/icon.png' });
 
     // and load the index.html of the app.
     win.loadFile('index.html');
