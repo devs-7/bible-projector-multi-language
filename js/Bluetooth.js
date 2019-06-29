a3b7f94ca8e00db86deceaf8b3eb06c36cd8a6c1
Silveira_L <46425069+SilveiraL@users.noreply.github.com>
2019-06-29 16:42:54 -0300

    Update Bluetooth.js

--- js/Bluetooth.js
+++ js/Bluetooth.js
@@ -1,1 +1,19 @@
 const Bluetooth = require('node-web-bluetooth');
+
+// async function connect() {
+//     const device = await Bluetooth.requestDevice({
+//         filters: [
+//             { services: ['heart_rate'] }
+//         ]
+//     });
+//     const server = await device.gatt.connect();
+//     const service = await server.getPrimaryService('heart_rate');
+//     const char = await service.getCharacteristic('heart_rate_measurement');
+//     await char.startNotifications();
+//     char.on('characteristicvaluechanged', (data) => {
+//         // parse heart-rate data here
+//     });
+//     await char.stopNotifications();
+//     await server.disconnect();
+// }
+// connect();
