ae0a54cff58d916ae9c2199b6bb2f6817850ce78
Silveira_L <luan.w.silveira@gmail.com>
2019-10-05 13:23:56 -0300

    arquivos organizados

---
+++ java/src/view/MenuView.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuView implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
