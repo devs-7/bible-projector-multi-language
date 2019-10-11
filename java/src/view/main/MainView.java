d1eb5308488dff6312a11ac43ebf2558871c6005
Silveira_L <luan.w.silveira@gmail.com>
2019-10-11 19:45:23 -0300

    java

--- java/src/view/main/MainView.java
+++ java/src/view/main/MainView.java
@@ -2,18 +2,26 @@ package view.main;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.EventObject;
+import java.util.ResourceBundle;
 
 
-public class MainView {
+public class MainView implements Initializable {
+    @FXML
+    private Pane paneThis;
     @FXML
     private TextField textFieldPesquisa;
     @FXML
@@ -21,7 +29,24 @@ public class MainView {
     @FXML
     private TextArea textArea;
 
+    private Stage stageThis;
     private Stage stageTextShow;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML
+    private void onKeyPressed(KeyEvent e) {
+        switch (e.getCode()) {
+            case F5:
+                if (stageTextShow.isShowing()) {
+                    stageTextShow.setFullScreen(true);
+                    System.out.println("Full");
+                } else show();
+                break;
+        }
+    }
 
     @FXML
     private void show() {
@@ -38,12 +63,7 @@ public class MainView {
     }
 
     @FXML
-    private void onKeyPressed(KeyEvent e) {
-        switch (e.getCode()) {
-            case F5:
-                show();
-                break;
-        }
+    private void pesquisar() {
+        System.out.println("Pesquisar");
     }
-
 }
