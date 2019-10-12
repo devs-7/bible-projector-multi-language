bafe114540b60840f246eda58dddc54a7a3dc2d3
Silveira_L <luan.w.silveira@gmail.com>
2019-10-12 00:07:24 -0300

    Update MainView.java

--- java/src/view/main/MainView.java
+++ java/src/view/main/MainView.java
@@ -3,7 +3,6 @@ package view.main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,7 +14,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.EventObject;
 import java.util.ResourceBundle;
 
 
