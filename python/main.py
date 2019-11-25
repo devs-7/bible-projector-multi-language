af76309e46a372acdc468c421227aa61bca6eebf
Silveira_L <luan.w.silveira@gmail.com>
2019-11-25 17:04:27 -0300

    .

--- python/main.py
+++ python/main.py
@@ -1,15 +1,30 @@
 import models.bible as bible
 import traceback
 
-while True:
-    try:
-        q = input("Digite sua pesquisa: ")
-        x = bible.query_one(q)
-        if x is not None:
-            print()
-            print(f"{x['liv']} {x['cap']}:{x['ver']}\n{x['text']}")
-            print()
-    except IndexError as e:
-        print('Pesquisa inválida')
-    except:
-        traceback.print_exc()
+from tkinter import *
+
+window = Tk()
+
+# Configuração da tela
+window.title("Bible projector")
+window.geometry('800x600')
+window.wm_state('zoomed')
+
+for i in range(10):
+    for j in range(10):
+        Label(text=f'{i + j}').grid(row=i, column=j)
+
+window.mainloop()
+
+# while True:
+#     try:
+#         q = input("Digite sua pesquisa: ")
+#         x = bible.query_one(q)
+#         if x is not None:
+#             print()
+#             print(f"{x['liv']} {x['cap']}:{x['ver']}\n{x['text']}")
+#             print()
+#     except IndexError as e:
+#         print('Pesquisa inválida')
+#     except:
+#         traceback.print_exc()
