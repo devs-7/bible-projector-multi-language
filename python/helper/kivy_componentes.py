ff089720a2cb04ed82dd2bbf2ed0c932198922cc
Silveira_L <luan.w.silveira@gmail.com>
2019-11-29 20:08:11 -0300

    venv kivy

---
+++ python/helper/kivy_componentes.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Label
+
+
+class Lista(BoxLayout):
+    def __init__(self, lista, **kwargs):
+        self.orientation = 'vertical'
+        super().__init__(**kwargs)
+        for item in lista:
+            self.add_widget(Label(text=item))
--- python/main.py
+++ python/main.py
@@ -1,20 +1,42 @@
 import models.bible as bible
 import traceback
 
-from tkinter import *
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
-window = Tk()
+from helper.kivy_componentes import *
+
+
+class Main(App):
+    def build(self):
+        box = BoxLayout(orientation='vertical')
+
+        pesquisar_button = Button(text='pesquisar', on_press=lambda x: print(x))
+        pesquisa_text_input = TextInput()
+
+        pesquisa_text_input.multiline = False
+        pesquisa_text_input.cursor_color = (0, 0, 0, 1)
+
+        box.add_widget(Lista(['1', '2', '3']))
+        box.add_widget(pesquisa_text_input)
+        box.add_widget(pesquisar_button)
+        return box
+
+
+Main().run()
 
 # Configuração da tela
-window.title("Bible projector")
-window.geometry('800x600')
-window.wm_state('zoomed')
-
-for i in range(10):
-    for j in range(10):
-        Label(text=f'{i + j}').grid(row=i, column=j)
-
-window.mainloop()
+# window.title("Bible projector")
+# window.geometry('800x600')
+# window.wm_state('zoomed')
+#
+# for i in range(10):
+#     for j in range(10):
+#         Label(text=f'{i + j}').grid(row=i, column=j)
+#
+# window.mainloop()
 
 # while True:
 #     try:
