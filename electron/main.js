217674ca3a67456edb78255436f64827fd811000
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-01-18 21:50:54 -0300

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,5 @@
 const electron = require('electron')
-const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow } = electron
 
 function createWindow() {
     let win = new BrowserWindow({
