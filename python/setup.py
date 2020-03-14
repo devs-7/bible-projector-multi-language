0b9ee048e0c594a265f21e078b30d9499b4c2287
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-03-14 02:05:03 -0300

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,11 @@ import os
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
+    os.path.join('main.py'),
     '--name=%s' % 'Projetor bíblico',
-    '--onefile',
+    '--icon=%s' % os.path.join('assets', 'img', 'icon.ico'),
+    '--add-data=%s' % os.path.join('data', 'dados.db;.', 'data'),
+    # '--add-binary=%s' % os.path.join('resource', 'path', '*.png'),
+    # '--onefile',
     '--windowed',
-    '--add-binary=%s' % os.path.join('resource', 'path', '*.png'),
-    '--add-data=%s' % os.path.join('resource', 'path', '*.txt'),
-    '--icon=%s' % os.path.join('resource', 'path', 'icon.ico'),
-    os.path.join('my_package', '__main__.py'),
 ])
