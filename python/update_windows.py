e0a6de6c561f480a46e712c1ffd2946d4020f26a
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-03-14 18:41:32 -0300

    Update update_windows.py

--- python/update_windows.py
+++ python/update_windows.py
@@ -1,9 +1,9 @@
 import os
 
 packages = ['main', 'projetor']
+current_path = os.path.abspath(os.getcwd())
 
-for package in packages:
-    current_path = os.path.abspath(os.getcwd())
+for package in os.listdir(os.path.join(current_path, 'ui')):
     main_view_ui_path = os.path.join(current_path, 'ui', package, 'window.ui')
     main_view_py_path = os.path.join(current_path, 'ui', package, 'window.py')
     os.system(f"pyuic5 {main_view_ui_path} -o {main_view_py_path}")
