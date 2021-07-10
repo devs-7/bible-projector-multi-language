0f4091023d332e175301b597eb3724c9acb66b63
Luan Willig Silveira <luan.w.silveira@gmail.com>
2021-07-10 17:33:34 -0300

    Update text_dao.py

--- python/src/dao/text_dao.py
+++ python/src/dao/text_dao.py
@@ -1,5 +1,9 @@
+from typing import List
 from src.models import Text
+from src.database import db
+
 
 class TextDAO:
-    def search(q: str):
-        
+    def search(self, q: str) -> List[Text]:
+        texts = db.session.query(Text).filter(Text.text.like(f'%{q}%')).all()
+        return texts
